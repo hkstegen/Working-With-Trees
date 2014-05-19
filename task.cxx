ca9fa5697cc02b017cf0a886a3dc7a9ccb7daaff
Hannah Stegen <hkstegen@mta.ca>
2014-05-19 17:15:37 +0200

    may 19

--- task.cxx
+++ task.cxx
@@ -5,6 +5,7 @@
 #include <TCanvas.h>
 #include <TROOT.h>
 #include <TH2.h>
+#include <TLorentzVector.h>
 
 void LearningTrees(Char_t* name = "Pi0_mc_out_250.root")
 	{
@@ -31,11 +32,38 @@ void LearningTrees(Char_t* name = "Pi0_mc_out_250.root")
 				
 			}
 			
-			canvas = new TCanvas("En_bm", "En_bm as function of GeV and MeV");
-			canvas->Divide(1,2);
+		canvas = new TCanvas("En_bm", "En_bm as function of GeV and MeV");
+		canvas->Divide(1,2);
 			
-			canvas->cd(1); En_bm_GeV->Draw();
-			canvas->cd(2); En_bm_MeV->Draw();
+		canvas->cd(1); En_bm_GeV->Draw();
+		canvas->cd(2); En_bm_MeV->Draw();
+		
+		Float_t Px_l0114, Py_l0114, Pz_l0114, Pt_l0114, En_l0114;
+			
+		tree->SetBranchAddress("Px_l0114", &Px_l0114);
+		tree->SetBranchAddress("Py_l0114", &Py_l0114);
+		tree->SetBranchAddress("Pz_l0114", &Pz_l0114);
+		tree->SetBranchAddress("Pt_l0114", &Pt_l0114);
+		tree->SetBranchAddress("En_l0114", &En_l0114);
+		
+		TLorentzVector v1;
+		TLorentzVector v2;
+		TLorentzVector v3;
+		TLorentzVector v4;
+		
+		Float_t Px1n, Py1n, Pz1n;
+		for (Int_t i=0;i<nentries; i++)
+			{
+				tree->GetEntry(i);
+				
+				Px1n=Px_l0114*Pt_l0114;
+				Py1n=Py_l0114*Pt_l0114;
+				Pt1n=Pz_l0114*Pt_l0114;
+				
+				v.SetPxPyPzE(Px1n,Py1n,Pz1n,En_l0114);
+			}
+			
+				
 		}
 		
 	
